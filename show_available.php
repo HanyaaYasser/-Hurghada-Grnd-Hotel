ae0d0c04459502c5b105bcc38b4c7cab118ff98b
marwanf8 <106749407+marwanf8@users.noreply.github.com>
2022-06-09 22:41:56 +0200

    Add files via upload

---
+++ show_available.php
@@ -0,0 +1,65 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+            table{
+    font-size: 20px;
+    color:black;
+
+}
+th{
+background-color: black;
+color : white;
+}
+
+            </style>
+    </head>
+    <body>
+        <?php include "receptionist1.php";?>
+        <div class = "av_ro">
+        <h1 style = "border:white; background-color: white; width: 100%; text-align: center;">
+            available rooms
+        </h1>
+    <?php
+    $conn = new mysqli("localhost","root","","hotel");
+    $query= "SELECT * FROM room WHERE Avalibilty = 'yes'";
+    $result = mysqli_query($conn,$query);
+    ?>
+    
+    <html>
+    <form method="post" action="">
+    <table class="table table-bordered table-hover">
+    <thead>
+    <tr>
+    
+        <th> RoomNo </th>
+        <th> Availabilty </th>
+        <th> Room Type </th>
+      
+         
+    </tr>
+    </thead>
+    <?php
+     
+    while($row = mysqli_fetch_array($result)) 
+    {
+        
+    ?>
+    <tr>
+      
+        <td><?= $row['RoomNo']; ?></td>
+        <td><?= $row['Avalibilty']; ?></td>
+        <td><?= $row['RoomTypeId']; ?></td>
+      
+         
+    </tr>
+    <?php
+     
+    }
+    ?>
+    </table>
+     
+    </form>
+    </html>
