800073d7adf612319ab12cb46589aa9439ff74b2
Mohamed-Said12 <99090723+Mohamed-Said12@users.noreply.github.com>
2022-06-10 14:47:57 +0200

    Add files via upload

---
+++ Promotion.php
@@ -0,0 +1,70 @@
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+
+</style>
+<?php
+
+session_start();
+
+$conn = new mysqli('localhost','root','','sign up/in');
+
+
+$query = "SELECT * FROM register WHERE Role='Receptionist'";
+$result = mysqli_query($conn,$query);
+if(isset($_POST['save']))
+{
+	$promote_ID=$_POST['check'];
+	$Promoted_ID=implode(',',$promote_ID);
+	
+	$QualityRole="QualityControl";
+	$sql="UPDATE register set Role='$QualityRole' WHERE ID='".$Promoted_ID."'";
+	mysqli_query($conn,$sql);
+}
+
+?>
+<html>
+<form method="post" action="">
+<table class="table table-bordered">
+<thead>
+<tr>
+
+	<th>First Name</th>
+
+	<th> Gender</th>
+	<th>Email</th>
+	<th>PhoneNumber</th>
+    <th>Role</th>
+	<th>Promote</th>
+	 
+</tr>
+</thead>
+<?php
+ 
+while($row = mysqli_fetch_array($result)) 
+{
+    
+?>
+<tr>
+  
+<td><?= $row['FirstName']; ?></td>
+
+	<td><?= $row['Gender']; ?></td>
+	<td><?=  $row['Email']; ?></td>
+	<td><?= $row['PhoneNumber']; ?></td>
+	<td><?= $row['Role']; ?></td>
+    <td><input type="checkbox" id="checkItem" name="check[]" value="<?php echo $row["ID"]; ?>"></td>
+	 
+</tr>
+<?php
+ 
+}
+?>
+</table>
+<p><button type="submit" class="btn btn-success" name="save">Promote</button></p>
+</form>
+
+</body>
+</html>
+</html>
